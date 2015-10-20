9da863a0491f15aa1287b9f14da93b225dfa1ade
Jon Malis <jmalis@live.com>
2015-10-19 19:52:04 -0700

    fixed routes changed staff and animal show views

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
   mount Refinery::Core::Engine, at: Refinery::Core.mounted_path
 
-  get 'pages#home'
+  # get 'pages#home'
 
-   root :to => 'pages#home'
+  # root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
