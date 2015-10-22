280f07c37036bffdf0e89a2386ce340d359f0b2e
Jon Malis <jmalis@live.com>
2015-10-22 10:00:26 -0700

    heroku junk

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = true
+
 
 module Myrescuesite
   class Application < Rails::Application
@@ -35,6 +35,6 @@ module Myrescuesite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = true
   end
 end
