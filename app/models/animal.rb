785581bc7c017b6304fdd6d86b928ccb11ad4ca7
Jon Malis <jmalis@live.com>
2015-10-27 17:33:32 -0700

    blog still has rss feed issues

--- app/models/animal.rb
+++
@@ -1,4 +0,0 @@
-class Animal < ActiveRecord::Base
-
-  acts_as_indexed :fields => [:name, :breed]
-end
--- config/initializers/refinery/search.rb
+++ config/initializers/refinery/search.rb
@@ -2,7 +2,8 @@ Refinery::Search.configure do |config|
    config.enable_for = ["Refinery::Page",
                         "Refinery::Animals::Animal",
                         "Refinery::Events::Event",
-                        "Refinery::Staffs::Staff"]
+                        "Refinery::Staffs::Staff",
+                        "Refinery::Blog::Post"]
   # config.page_url = "/search"
   # config.results_per_page = 10
 end
