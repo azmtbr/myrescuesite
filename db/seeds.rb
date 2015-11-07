2d655da672c8c2b5f230e7c13e5c7823d97eca9b
Jon Malis <jmalis@live.com>
2015-11-06 17:17:26 -0700

    added post ability to commumity board

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,30 @@ Refinery::Blog::Engine.load_seed
 
 # Added by Refinery CMS Forums extension
 Refinery::Forums::Engine.load_seed
+
+include RandomData
+
+ # Create Posts
+ 50.times do
+ # #1
+   Post.create!(
+ # #2
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
+
+ # Create Comments
+ # #3
+ 100.times do
+   Comment.create!(
+ # #4
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
--- vendor/extensions/forums/app/controllers/refinery/forums/forums_controller.rb
+++ vendor/extensions/forums/app/controllers/refinery/forums/forums_controller.rb
@@ -8,15 +8,33 @@ module Refinery
       def index
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @forum in the line below:
-        present(@page)
+        present(@forum)
+        @posts = Post.all
       end
 
       def show
-        @forum = Forum.find(params[:id])
+        @post = Post.find(params[:id])
+      end
 
-        # you can use meta fields from your model instead (e.g. browser_title)
-        # by swapping @page for @forum in the line below:
-        present(@page)
+      def new
+        @post = Post.new
+      end
+
+      def create
+        @post = Post.new
+        @post.title = params[:post][:title]
+        @post.body = params[:post][:body]
+
+        if @post.save
+          flash[:notice] = "Post was saved."
+          redirect_to @post
+        else
+          flash[:error] = "There was an error saving the post. Please try again."
+          render :new
+        end
+      end
+
+      def edit
       end
 
     protected
